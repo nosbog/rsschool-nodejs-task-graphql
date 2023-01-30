4f2e139aff17dd1e652a11d4afb632ec219401bc
Svetlana-85 <Svetlanka-85@yandex.ru>
2023-01-30 20:21:10 +0500

    feat: add gql requests: 2.1. Get users, profiles, posts, memberTypes

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,7 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import { graphql, GraphQLList, GraphQLObjectType, GraphQLSchema } from 'graphql';
 import { graphqlBodySchema } from './schema';
+import { MemberType, PostType, Profile, User } from './types';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
@@ -11,8 +13,44 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: graphqlBodySchema,
       },
     },
-    async function (request, reply) {}
+    async function (request, reply) {
+      const sourceReq = request.body.query || request.body.mutation || "";
+
+      const queryAll = new GraphQLObjectType({
+        name: "query",
+        fields: {
+          users: {
+            type: new GraphQLList(User),
+            async resolve (parent) {
+              if (request.body.variables) return await fastify.db.users.findOne({key: "id", equals: parent.id});
+              return fastify.db.users.findMany();
+            },
+          },
+          profiles: {
+            type: new GraphQLList(Profile),
+            resolve() {
+              return fastify.db.profiles.findMany();
+            },
+          },
+          posts: {
+            type: new GraphQLList(PostType),
+            resolve() {
+              return fastify.db.posts.findMany();
+            },
+          },
+          memberTypes: {
+            type: new GraphQLList(MemberType),
+            resolve() {
+              return fastify.db.memberTypes.findMany();
+            },
+          },
+        }
+      }); 
+      const schema = new GraphQLSchema({ query: queryAll })
+      return await graphql({ schema, source: sourceReq, variableValues: request.body.variables });
+    }
   );
+  
 };
 
 export default plugin;
---
+++ src/routes/graphql/types.ts
@@ -0,0 +1,47 @@
+import { GraphQLList, GraphQLObjectType, GraphQLString } from "graphql";
+
+export const User = new GraphQLObjectType({
+    name: "user",
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        subscribedToUserIds: { type: new GraphQLList ( GraphQLString ) }
+    }
+  });
+  
+export const Profile = new GraphQLObjectType({
+    name: "profile",
+    fields: {
+      id: { type: GraphQLString },
+      avatar: { type: GraphQLString },
+      sex: { type: GraphQLString },
+      birthday: { type: GraphQLString },
+      country: { type: GraphQLString },
+      street: { type: GraphQLString },
+      city: { type: GraphQLString },
+      memberTypeId: { type: GraphQLString },
+      userId: { type: GraphQLString },
+    }
+  });
+  
+export const PostType = new GraphQLObjectType({
+    name: "p",
+    fields: {
+      id: { type: GraphQLString },
+      title: { type: GraphQLString },
+      content: { type: GraphQLString },
+      userId: { type: GraphQLString },
+    }
+  });
+  
+export const MemberType = new GraphQLObjectType({
+    name: "memberType",
+    fields: {
+      id: { type: GraphQLString },
+      discount: { type: GraphQLString },
+      monthPostsLimit: { type: GraphQLString },
+    }
+  });
+  
