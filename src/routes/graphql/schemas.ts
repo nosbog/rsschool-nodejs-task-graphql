3522cef86e70d833cc15181832c1459d3126406c
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2023-07-17 13:44:04 +0200

    feat: init task

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,5 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLBoolean, GraphQLFloat, GraphQLObjectType, GraphQLInt, GraphQLSchema, GraphQLString, GraphQLID } from 'graphql';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +19,78 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+ const UserType = new GraphQLObjectType({
+   name: "User",
+   fields: () => ({
+     id: {type: GraphQLID},
+     name: {type: GraphQLString},
+     balance: {type: GraphQLFloat},
+     profile: {type: ProfileType},
+     posts: {type: PostType}
+    // profile: {},
+    // posts: {},
+    // userSubscribedTo: {},
+    // subscribedToUser: {}
+   })
+ })
+
+const SubscribersOnAuthorsType = new GraphQLObjectType({
+  name: 'SubscribersOnAuthors',
+  fields: () => ({
+    subscriber: {type: UserType},   
+    author: {type: UserType},  
+  }) 
+})
+
+const ProfileType = new GraphQLObjectType({
+  name: "Profile",
+  fields: () => ({
+    id: {type: GraphQLID},
+    isMale: {type: GraphQLBoolean},
+    yearOfBirth: {type: GraphQLInt},
+
+   // profile: {},
+   // posts: {},
+   // userSubscribedTo: {},
+   // subscribedToUser: {}
+  })
+})
+
+const PostType = new GraphQLObjectType({
+  name: "Post",
+  fields: () => ({
+    id: {type: GraphQLID},
+    title: {type: GraphQLString},
+    content: {type: GraphQLString},
+    author: {type: UserType},
+  })
+})
+
+const MemberType = new GraphQLObjectType({
+  name: "Member",
+  fields: () => ({
+    id: {type: GraphQLID},
+    discount: {type: GraphQLFloat},
+    postsLimitPerMonth: {type: GraphQLInt},
+    profiles: {type: ProfileType},
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: () => ({
+    user: {
+      type: UserType,
+      args: {id: {type: GraphQLID}},
+      resolve: (parent, args) => {
+        return args.id
+      } 
+    }
+  })
+})
+
+export const schema = new GraphQLSchema({
+  query: RootQuery
+})
+
