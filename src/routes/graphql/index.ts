6d0195d2b62131fc5538e845036b18711fefffc4
hi-bi <qds315@gmail.com>
2023-07-19 14:04:47 +0300

    init: gql-queries.Get all resources implemented

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,14 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import { createGqlResponseSchema, gqlResponseSchema, gqlSchema } from './schemas.js';
 import { graphql } from 'graphql';
 
+export let gqlPrisma: any = {};
+
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const {prisma} = fastify;
+
+  gqlPrisma = {prisma}.prisma;
+   
   fastify.route({
     url: '/',
     method: 'POST',
@@ -12,10 +18,19 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
-      return {};
+    async handler(req, rep) {
+
+      const query = req.body?.query;
+
+      const res = await graphql({schema: gqlSchema, source: query}).then((result) => {
+
+        return result
+      });
+
+      rep.send(res) ;
     },
   });
+
 };
 
 export default plugin;
--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,11 +1,14 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLBoolean, GraphQLFloat, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLUnionType } from 'graphql';
+import { gqlPrisma } from './index.js';
+import { UUIDType } from './types/uuid.js'
 
-export const gqlResponseSchema = Type.Partial(
+  export const gqlResponseSchema = Type.Partial(
   Type.Object({
     data: Type.Any(),
     errors: Type.Any(),
   }),
-);
+);  
 
 export const createGqlResponseSchema = {
   body: Type.Object(
@@ -18,3 +21,105 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+const MemberType = new GraphQLObjectType ({
+  name: 'memberType',
+  fields: () => ({
+    id: {
+      type: GraphQLString!
+    },
+    discount: {
+      type: GraphQLFloat!
+    },
+    postsLimitPerMonth: {
+      type: GraphQLInt!
+    }  
+  })
+});
+
+const Post = new GraphQLObjectType ({
+  name: 'post',
+  fields: () => ({
+    id: {
+      type: UUIDType!
+    },
+    title: {
+      type: GraphQLInt
+    },
+    content: {
+      type: GraphQLString
+    }  
+  })
+});
+
+const User = new GraphQLObjectType ({
+  name: 'user',
+  fields: () => ({
+    id: {
+      type: UUIDType!
+    },
+    name: {
+      type: GraphQLString
+    },
+    balance: {
+      type: GraphQLFloat
+    }  
+  })
+});
+
+const Profile = new GraphQLObjectType ({
+  name: 'profile',
+  fields: () => ({
+    id: {
+      type: UUIDType!
+    },
+    isMale: {
+      type: GraphQLBoolean!
+    },
+    yearOfBirth: {
+      type: GraphQLInt!
+    }  
+  })
+});
+
+const RootQuery  = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    memberTypes: {
+      type: new GraphQLList(MemberType),
+      async resolve () {
+        const res = await gqlPrisma.memberType.findMany();
+
+        return res;
+      }
+    },
+    posts: {
+      type: new GraphQLList(Post),
+      async resolve () {
+        const res = await gqlPrisma.post.findMany();
+
+        return res;
+      }
+    },
+    users: {
+      type: new GraphQLList(User),
+      async resolve () {
+        const res = await gqlPrisma.user.findMany();
+
+        return res;
+      }
+    },
+    profiles: {
+      type: new GraphQLList(Profile),
+      async resolve () {
+        const res = await gqlPrisma.profile.findMany();
+
+        return res;
+      }
+    }
+  }
+})
+
+export const gqlSchema = new GraphQLSchema({
+  query: RootQuery
+});
