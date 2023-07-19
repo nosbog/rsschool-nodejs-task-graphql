c822876e9899f7f36deb84ef34e99f8ebecf2ae8
emp74ark <emp74ark@gmail.com>
2023-07-19 14:08:45 +0200

    feat: depth limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import {FastifyPluginAsyncTypebox} from '@fastify/type-provider-typebox';
 import {createGqlResponseSchema, gqlResponseSchema} from './schemas.js';
-import {graphql} from 'graphql';
+import {graphql, parse, validate} from 'graphql';
 import {rootSchema} from "./rootSchema.js";
+import depthLimit from 'graphql-depth-limit'
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     fastify.route({
@@ -15,15 +16,24 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         },
         async handler(req) {
             const {query, variables} = req.body;
+
+            const checkLimit = validate(rootSchema, parse(query), [depthLimit(5)])
+
             const result = await graphql({
                 schema: rootSchema,
                 source: query,
                 variableValues: variables,
             });
+
             console.log('VARIABLES: ', variables); //todo: remove
             console.log('QUERY: ', result); // todo: remove
             console.log('ERRORS: ', result.errors) // todo: remove
-            return {data: result.data, errors: result.errors};
+            console.log('LIMIT: ', checkLimit) // todo: remove
+
+            return {
+                data: checkLimit ? null : result.data,
+                errors: checkLimit ? checkLimit : result.errors
+            };
         },
     });
 };
