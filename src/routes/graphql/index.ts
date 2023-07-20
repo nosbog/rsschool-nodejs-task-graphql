7da172f37854d2b0e8d1c873418d5fdac2b441a8
necket <nik.sherbakov.01@gmail.com>
2023-07-21 01:00:12 +0300

    add depthLimit validation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 import { schema } from './schema/schema.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
@@ -14,6 +15,9 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const errors = validate(schema, parse(req.body.query), [depthLimit(5)]);
+      if (errors.length > 0) return { data: null, errors: errors };
+
       return await graphql({
         schema,
         source: String(req.body.query),
