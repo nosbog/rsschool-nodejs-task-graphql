4fa58264fcd39d99a18b4d30ce24ae5f65510ad8
Agnia Staravoitava <78143065+AgniaStaravoitava@users.noreply.github.com>
2023-07-22 16:25:14 +0200

    init: add graphql call

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,6 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { buildSchema, graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,7 +13,18 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const schemaSDL = `
+        type Query {
+          hello: String
+        }
+    `;
+
+      const schema = buildSchema(schemaSDL);
+      const { query, variables } = req.body;
+      const result = await graphql({ schema, source: query, variableValues: variables });
+
+      console.log(result);
+      return result;
     },
   });
 };
