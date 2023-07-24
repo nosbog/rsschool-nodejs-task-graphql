d90e97793159d51f0a369d496f8923a5f310fbd8
Madiyar Kenzhegulov <madiyar.kenzhegulov@gmail.com>
2023-07-25 02:12:48 +0600

    chore: nice try

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,24 @@
+import 'graphql-import-node';
+
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, validate, parse, buildASTSchema } from 'graphql';
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import * as rawSchema from './schema.graphql';
+import { resolve, dirname } from 'node:path';
+
+const root = {
+  users: (a, b, c) => {
+    console.log(a, b, c);
+
+    return 'kek';
+  },
+};
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,7 +29,32 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const { query, variables } = req.body;
+      const parsedQuery = parse(query);
+      const schema = buildASTSchema(rawSchema);
+
+      const errors = validate(schema, parsedQuery);
+
+      if (errors.length) {
+        return {
+          errors,
+        };
+      }
+
+      const executionResult = await graphql({
+        schema: schema,
+        source: query,
+        rootValue: root,
+        variableValues: variables,
+        contextValue: {
+          prisma,
+        },
+      });
+
+      return {
+        data: executionResult.data,
+        errors: executionResult.errors,
+      };
     },
   });
 };
