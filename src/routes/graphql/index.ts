879254c9b5a26808dbf9864d386543f682348b22
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-07-24 22:05:27 +0500

    queries added

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,11 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import graphQLSchema from './graphQLSchema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
   fastify.route({
     url: '/',
     method: 'POST',
@@ -12,8 +15,24 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
-      return {};
+    async handler(req, reply) {
+      const { query, variables } = req.body;
+
+      const validationErrors = validate(graphQLSchema, parse(query), [depthLimit(5)]);
+
+      if (validationErrors.length > 0) {
+        await reply.send({ errors: validationErrors });
+      }
+
+      const res = await graphql({
+        schema: graphQLSchema,
+        source: query,
+        variableValues: variables,
+        contextValue: {
+          prisma,
+        },
+      });
+      return res;
     },
   });
 };
