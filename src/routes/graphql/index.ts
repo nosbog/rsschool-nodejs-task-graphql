83787b2cedfae0cb0cc481cb96a6d4ce44e611ab
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-07-24 12:06:32 +0300

    feat: write validation query with depthLimit 5

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,9 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { GraphQLSchema, graphql } from 'graphql';
+import { GraphQLSchema, graphql, parse, validate } from 'graphql';
 import { queries } from './queriesShemas.js';
 import { mutations } from './mutations.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
 
@@ -23,14 +24,19 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
            mutation: mutations
       });
 
+      const errors = validate(queriesShema, parse(req.body.query), [depthLimit(5)]);
+
+      if(errors.length) {
+        return { errors };
+      }
+
       const res = await graphql({
         schema: queriesShema,
-        source: String(req.body.query),
+        source: req.body.query,
         contextValue: fastify,
         variableValues: req.body.variables,
       });
       return res;
-      //{data: res.data}
     },
   });
 };
