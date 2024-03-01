bbbf9ba3720d2893d8c31d354040a9219deffa39
Vera Poletaeva <poletaeva.vera.vladimirovna@gmail.com>
2024-02-29 19:24:32 -0500

    feat: find by ID

--- src/routes/graphql/rootQuery.ts
+++ src/routes/graphql/rootQuery.ts
@@ -1,5 +1,7 @@
-import { GraphQLList, GraphQLObjectType } from 'graphql';
-import { Context, MemberType } from './types/types.js';
+import { GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLScalarType } from 'graphql';
+import { Context, MTIdType, MemberType, PostType, ProfileType, UserType } from './types/types.js';
+import { UUIDType } from './types/uuid.js';
+import { httpErrors } from '@fastify/sensible';
 
 export const RootQuery = new GraphQLObjectType<unknown, Context>({
   name: 'RootQueryType',
@@ -10,5 +12,86 @@ export const RootQuery = new GraphQLObjectType<unknown, Context>({
         return ctx.prisma.memberType.findMany();
       },
     },
+    posts: {
+        type: new GraphQLList(PostType),
+        resolve: async (_obj, _args, ctx: Context) => {
+          return ctx.prisma.post.findMany();
+        },
+      },
+    users: {
+        type: new GraphQLList(UserType),
+        resolve: async (_obj, _args, ctx: Context) => {
+          return ctx.prisma.user.findMany();
+        },
+      },  
+    profiles: {
+        type: new GraphQLList(ProfileType),
+        resolve: async (_obj, _args, ctx: Context) => {
+          return ctx.prisma.profile.findMany();
+        },
+      }, 
+    memberType: {
+        type: new GraphQLNonNull(MemberType),
+        args: {
+            id: { type: new GraphQLNonNull(MTIdType) },
+        },
+        resolve: async (_obj, args: { id: string }, ctx: Context) => {
+          return ctx.prisma.memberType.findUnique({
+            where: {
+              id: args.id,
+            }});
+        },
+      },  
+      post: {
+        type: PostType,
+        args: {
+            id: { type: new GraphQLNonNull(UUIDType) },
+        },
+        resolve: async (_obj, args: { id: string }, ctx: Context) => {
+            console.log('running post API' );
+            let post: unknown;
+            try {
+                post = await ctx.prisma.post.findUnique({
+                    where: {
+                        id: args.id,
+                    }
+                }); 
+             console.log(' FOUND' );
+            } 
+            catch (err)  {
+                console.log('NOT FOUND' );
+                throw httpErrors.notFound();
+            }
+            return post || {post: null};
+        },
+      },    
+      user: {
+        type: new GraphQLNonNull(UserType),
+        args: {
+            id: { type: new GraphQLNonNull(UUIDType) },
+        },
+        resolve: async (_obj, args: { id: string }, ctx: Context) => {
+            const user = await ctx.prisma.user.findUnique({
+                where: {
+                  id: args.id,
+                }
+              });
+              return user || null; 
+        },
+      },  
+      profile: {
+        type: new GraphQLNonNull(ProfileType),
+        args: {
+            id: { type: new GraphQLNonNull(UUIDType) },
+        },
+        resolve: async (_obj, args: { id: string }, ctx: Context) => {
+            const profile = await ctx.prisma.profile.findUnique({
+                where: {
+                  id: args.id,
+                }
+              });
+              return profile || null; 
+        },
+      },  
   },
 });
--- src/routes/graphql/types/types.ts
+++ src/routes/graphql/types/types.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { GraphQLObjectType, GraphQLString, GraphQLFloat, GraphQLInt } from "graphql";
+import { GraphQLObjectType, GraphQLString, GraphQLFloat, GraphQLInt, GraphQLBoolean, GraphQLEnumType, GraphQLNonNull } from "graphql";
+import { MemberTypeId } from '../../member-types/schemas.js';
 
 const memberTypeFields = {
     id: { type: GraphQLString },
@@ -12,6 +13,56 @@ const memberTypeFields = {
     fields: memberTypeFields,
   });
 
+  const postTypeFields = {
+    id: { type:  new GraphQLNonNull(GraphQLString) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(GraphQLString) },
+  };
+  
+  export const PostType = new GraphQLObjectType({
+    name: 'PostType', 
+    fields: postTypeFields,
+  });
+
+  const userTypeFields = {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    balance: { type: GraphQLInt },
+  };
+  
+  export const UserType = new GraphQLObjectType({
+    name: 'UserType', 
+    fields: userTypeFields,
+  });
+
+  const profileTypeFields = {
+    id: { type: GraphQLString },
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+    userId: { type: GraphQLString },
+    memberTypeId: { type: GraphQLString },
+  };
+  
+  export const ProfileType = new GraphQLObjectType({
+    name: 'ProfileType', 
+    fields: profileTypeFields,
+  });
+
+  const TypeIdValues = {
+    'basic': {
+      value: MemberTypeId.BASIC
+    },
+    'business': {
+      value: MemberTypeId.BUSINESS
+    }
+  }
+
+  export const MTIdType = new GraphQLEnumType({
+    name: 'MemberTypeId', 
+    values: TypeIdValues,
+  });
+  
 
   export type Context = {
     prisma: PrismaClient;
