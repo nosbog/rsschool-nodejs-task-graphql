3585113a2b9c9eaf5dae02a9292dac9b811a61f5
Anastasia445 <avolkova@asist-lab.com>
2024-03-03 16:06:04 +0300

    created plugins

--- src/plugins/sensible.ts
+++ src/plugins/sensible.ts
@@ -1,8 +1,6 @@
 import fp from 'fastify-plugin';
 import sensible from '@fastify/sensible';
 
-export const sensiblePluginTag = 'sensiblePluginTag';
-
-export default fp(async (fastify) => fastify.register(sensible), {
-  name: sensiblePluginTag,
+export default fp(async (fastify): Promise<void> => {
+  fastify.register(sensible);
 });
