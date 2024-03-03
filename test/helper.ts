8975798ab19d2edbc1084d8909d3c48a3b7db41c
Anastasia445 <avolkova@asist-lab.com>
2024-03-03 16:10:17 +0300

    test updated

--- test/helper.ts
+++ test/helper.ts
@@ -1,25 +1,22 @@
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { FastifyInstance } from 'fastify';
-import tap from 'tap';
-// @ts-ignore
-import helper from 'fastify-cli/helper.js';
+const helper = require('fastify-cli/helper.js');
+import * as path from 'path';
+import * as tap from 'tap';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+export type Test = typeof tap['Test']['prototype'];
 
-export type Test = (typeof tap)['Test']['prototype'];
+const AppPath = path.join(__dirname, '..', 'src', 'app.ts');
 
-const AppPath = join(__dirname, '..', 'dist', 'app.js');
-
-function config() {
+async function config() {
   return {};
 }
 
 async function build(t: Test) {
   const argv = [AppPath];
-  const app = (await helper.build(argv, config())) as FastifyInstance;
-  t.teardown(() => app.close());
+
+  const app = await helper.build(argv, await config());
+
+  t.teardown(() => void app.close());
+
   return app;
 }
 
