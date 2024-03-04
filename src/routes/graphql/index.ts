68ef53c21f2e59ec2c2c7952899e131eb0d2ceae
Alinochka07 <alina011988@icloud.com>
2024-03-04 10:33:07 +0600

    init, feat:graphql

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,21 +1,56 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { GraphQLSchema, graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
+
+
+interface GqlRequestBody {
+  query?: string;
+  variables?: Record<string, unknown>;
+}
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
+
   fastify.route({
     url: '/',
-    method: 'POST',
+    method: ['POST'],
     schema: {
       ...createGqlResponseSchema,
       response: {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
-      return {};
+    async handler(request, _) {
+      const { query, variables }: GqlRequestBody = request.body;
+
+      try {
+        const result = await graphql({
+          schema: schema, 
+          source: query, 
+          variableValues: variables, 
+          contextValue: { prisma }, 
+           
+        });
+        const errors = validate(schema, parse(query), [depthLimit(5)]);
+        if (errors.length > 0) {
+          return { errors }
+        } else {
+          return result;
+        }
+
+      } catch (error) {
+        console.error('GraphQL error:', error);
+        throw new Error('Internal server error');
+      }
     },
   });
 };
+
+export const schema = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutations
+})
 
 export default plugin;
