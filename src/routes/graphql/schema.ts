c78c0df6964205e574ab8759ed096cf04464b6a5
demos-prog <demosne@gmail.com>
2024-10-26 12:42:47 +0300

    feat: user post  - start

--- src/routes/graphql/schema.ts
+++ src/routes/graphql/schema.ts
@@ -1,4 +1,44 @@
-import { GraphQLSchema } from 'graphql';
+import {
+  GraphQLFloat,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
+import { UUIDType } from './types/uuid.js';
+
+const CreateUserInput = new GraphQLObjectType({
+  name: 'CreateUserInput',
+  fields: {
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+  },
+});
+
+const ChangeUserInput = new GraphQLObjectType({
+  name: 'ChangeUserInput',
+  fields: {
+    name: { type: GraphQLString },
+    balance: { type: GraphQLFloat },
+  },
+});
+
+const CreatePostInput = new GraphQLObjectType({
+  name: 'CreatePostInput',
+  fields: {
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(UUIDType) },
+  },
+});
+
+const ChangePostInput = new GraphQLObjectType({
+  name: 'ChangePostInput',
+  fields: {
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+  },
+});
 
 const schema = new GraphQLSchema({
   query: null,
