a8becd5458c55858cf3ce81af3eeb5b1bca54950
demos-prog <demosne@gmail.com>
2024-10-26 14:04:21 +0300

    inputs

--- src/routes/graphql/schema.ts
+++ src/routes/graphql/schema.ts
@@ -1,12 +1,18 @@
 import {
+  GraphQLBoolean,
+  GraphQLEnumType,
   GraphQLFloat,
+  GraphQLInt,
+  GraphQLList,
   GraphQLNonNull,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
 } from 'graphql';
 import { UUIDType } from './types/uuid.js';
+import { MemberTypeId } from '../member-types/schemas.js';
 
+// ++
 const CreateUserInput = new GraphQLObjectType({
   name: 'CreateUserInput',
   fields: {
@@ -15,6 +21,7 @@ const CreateUserInput = new GraphQLObjectType({
   },
 });
 
+//+
 const ChangeUserInput = new GraphQLObjectType({
   name: 'ChangeUserInput',
   fields: {
@@ -23,6 +30,7 @@ const ChangeUserInput = new GraphQLObjectType({
   },
 });
 
+//+
 const CreatePostInput = new GraphQLObjectType({
   name: 'CreatePostInput',
   fields: {
@@ -32,12 +40,80 @@ const CreatePostInput = new GraphQLObjectType({
   },
 });
 
+//+
+const MemberTypeIdType = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  values: {
+    BASIC: { value: MemberTypeId.BASIC },
+    BUSINESS: { value: MemberTypeId.BUSINESS },
+  },
+});
+
+//+
 const ChangePostInput = new GraphQLObjectType({
   name: 'ChangePostInput',
   fields: {
     title: { type: GraphQLString },
     content: { type: GraphQLString },
   },
+});
+
+//+
+const CreateProfileInput = new GraphQLObjectType({
+  name: 'CreateProfileInput',
+  fields: {
+    isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+    yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+    memberTypeId: { type: new GraphQLNonNull(MemberTypeIdType) },
+    userId: { type: new GraphQLNonNull(UUIDType) },
+  },
+});
+
+//+
+const Post = new GraphQLObjectType({
+  name: 'Post',
+  fields: {
+    id: { type: new GraphQLNonNull(UUIDType) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(UUIDType) },
+  },
+});
+
+//+
+const MemberType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: {
+    id: { type: new GraphQLNonNull(MemberTypeIdType) },
+    discount: { type: new GraphQLNonNull(GraphQLFloat) },
+    postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) },
+  },
+});
+
+//+
+const Profile = new GraphQLObjectType({
+  name: 'Profile',
+  fields: {
+    id: { type: new GraphQLNonNull(UUIDType) },
+    isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+    yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+    userId: { type: new GraphQLNonNull(UUIDType) },
+    memberTypeId: { type: new GraphQLNonNull(MemberTypeIdType) },
+  },
+});
+
+//+
+const User = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    profile: { type: Profile },
+    posts: { type: new GraphQLList(Post) },
+    userSubscribedTo: { type: new GraphQLList(User) },
+    subscribedToUser: { type: new GraphQLList(User) },
+  }),
 });
 
 const schema = new GraphQLSchema({
