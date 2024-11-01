423ab79788a13a2e48144d6625c133dd06250605
Gunsnfnr <a.j.p.010203@gmail.com>
2024-11-01 23:44:58 +0300

    feat: add posts, users and profiles fields

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql, GraphQLList, GraphQLObjectType, GraphQLSchema } from 'graphql';
-import { memberTypes } from './types/types.js';
+import { memberTypes, posts, profiles, users } from './types/types.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -32,6 +32,24 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
           type: new GraphQLList(memberTypes),
           resolve: async () => {
             return prisma.memberType.findMany();
+          },
+        },
+        posts: {
+          type: new GraphQLList(posts),
+          resolve: async () => {
+            return prisma.post.findMany();
+          },
+        },
+        users: {
+          type: new GraphQLList(users),
+          resolve: async () => {
+            return prisma.user.findMany();
+          },
+        },
+        profiles: {
+          type: new GraphQLList(profiles),
+          resolve: async () => {
+            return prisma.profile.findMany();
           },
         },
       },
--- src/routes/graphql/types/types.ts
+++ src/routes/graphql/types/types.ts
@@ -1,4 +1,11 @@
-import { GraphQLFloat, GraphQLInt, GraphQLObjectType, GraphQLString } from 'graphql';
+import {
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
+import { UUIDType } from './uuid.js';
 
 export const memberTypes = new GraphQLObjectType({
   name: 'MemberTypes',
@@ -11,6 +18,60 @@ export const memberTypes = new GraphQLObjectType({
     },
     postsLimitPerMonth: {
       type: GraphQLInt,
+    },
+  }),
+});
+
+export const posts = new GraphQLObjectType({
+  name: 'Posts',
+  fields: () => ({
+    id: {
+      type: UUIDType,
+    },
+    title: {
+      type: UUIDType,
+    },
+    content: {
+      type: UUIDType,
+    },
+    authorId: {
+      type: UUIDType,
+    },
+  }),
+});
+
+export const users = new GraphQLObjectType({
+  name: 'Users',
+  fields: () => ({
+    id: {
+      type: UUIDType,
+    },
+    name: {
+      type: UUIDType,
+    },
+    balance: {
+      type: GraphQLFloat,
+    },
+  }),
+});
+
+export const profiles = new GraphQLObjectType({
+  name: 'Profiles',
+  fields: () => ({
+    id: {
+      type: UUIDType,
+    },
+    isMale: {
+      type: GraphQLBoolean,
+    },
+    yearOfBirth: {
+      type: GraphQLInt,
+    },
+    userId: {
+      type: UUIDType,
+    },
+    memberTypeId: {
+      type: GraphQLString,
     },
   }),
 });
