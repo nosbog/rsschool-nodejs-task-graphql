a5c9b2082a0168fd5eb41e4a8a574aee1330cf75
HamitMagic <Hamit.Magic@gmail.com>
2025-05-21 16:54:01 +0500

    add main types

--- src/routes/graphql/mySchema.ts
+++ src/routes/graphql/mySchema.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { Mutation, Query } from './types/main.js';
+import { GraphQLSchema } from 'graphql';
 
 function createSchema(prisma: PrismaClient) {
     return new GraphQLSchema({
-
+        query: Query,
+        mutation: Mutation,
     });
 }
-
-
 
 export default createSchema;
