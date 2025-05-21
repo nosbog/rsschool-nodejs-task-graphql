40394094d14981b69bd27a42419c683b6e2fa37a
HamitMagic <Hamit.Magic@gmail.com>
2025-05-21 16:54:23 +0500

    add main types

---
+++ src/routes/graphql/types/main.ts
@@ -0,0 +1,15 @@
+import { GraphQLObjectType } from "graphql";
+
+export const Query = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        
+    }
+})
+
+export const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        
+    }
+})
