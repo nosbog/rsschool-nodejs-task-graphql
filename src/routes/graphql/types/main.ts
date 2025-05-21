bb21e65ff69dc2ca3967893d99c72ed5c8a98ed3
HamitMagic <Hamit.Magic@gmail.com>
2025-05-21 17:19:27 +0500

    feat: add member type

--- src/routes/graphql/types/main.ts
+++ src/routes/graphql/types/main.ts
@@ -1,9 +1,25 @@
-import { GraphQLObjectType } from "graphql";
+import { GraphQLList, GraphQLObjectType } from "graphql";
+import {MemberType, MemberTypeId} from "./member-type.js";
+import { PrismaClient } from '@prisma/client';
+
+interface Context {
+    prisma: PrismaClient;
+}
 
 export const Query = new GraphQLObjectType({
     name: 'Query',
     fields: {
-        
+        memberTypes: {
+            type: new GraphQLList(MemberType),
+            resolve: async (_: unknown, args: unknown, context: Context) => await context.prisma.memberType.findMany(),
+        },
+        memberType: {
+            type: MemberType,
+            args: {
+                id: { type: MemberTypeId }
+            },
+            resolve: async (_: unknown, args: { id: string }, context: Context) => await context.prisma.memberType.findUnique({ where: { id: args.id } }),
+        }
     }
 })
 
