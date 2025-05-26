11920256049594f0598a985a9d8a1c0829f801d2
Dima Usov <dmitry.usov@yahoo.com>
2025-05-26 14:01:37 +0400

    feat: add depth limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,12 +1,15 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
 import { rootSchema } from './root.schema.js';
 import { userDataLoader } from './user/user.dataloader.js';
 import { RequestContext } from './types/request-context.js';
 import { memberTypeDataLoader } from './member-type/member-type.dataloader.js';
 import { profileDataLoader, userProfileDataLoader } from './profile/profile.dataloader.js';
 import { postDataLoader, userPostsDataLoader } from './post/post.dataloader.js';
+import depthLimit from 'graphql-depth-limit';
+
+const QUERIES_DEPTH_LIMIT = 5;
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -22,6 +25,13 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req) {
       const { query, variables } = req.body;
+
+      const validationErrors = validate(rootSchema, parse(query), [
+        depthLimit(QUERIES_DEPTH_LIMIT),
+      ]);
+      if (validationErrors.length) {
+        return { data: null, errors: validationErrors };
+      }
 
       const contextValue: RequestContext = {
         prismaClient: fastify.prisma,
