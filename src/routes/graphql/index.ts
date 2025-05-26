f96e846506a96153d6f0159172fe0aabdb5a9644
Anton Maltsev <Anton.Maltsev@McKesson.com>
2025-05-26 14:19:23 +0300

    feat: add all needed types

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,32 +1,82 @@
 import {FastifyPluginAsyncTypebox} from '@fastify/type-provider-typebox';
 import {createGqlResponseSchema, gqlResponseSchema} from './schemas.js';
-import {graphql, GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLList, GraphQLID} from 'graphql';
+import {
+    graphql,
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
+    GraphQLList,
+    GraphQLID,
+    GraphQLEnumType,
+    GraphQLFloat,
+    GraphQLInt
+} from "graphql";
+
+// import {
+//     graphql, GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLList,
+//     GraphQLID,
+//     GraphQLEnumType,
+//     GraphQLFloat,
+//     GraphQLInt
+// } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     const {prisma} = fastify;
 
-    const UserType = new GraphQLObjectType({
-        name: "User",
-        fields: () => ({
-            id: {type: GraphQLID},
-            name: {type: GraphQLString},
-        }),
+    const UUID = GraphQLString;
+
+    const MemberTypeId = new GraphQLEnumType({
+        name: "MemberTypeId",
+        values: {
+            BASIC: {value: "BASIC"},
+            BUSINESS: {value: "BUSINESS"}
+        }
     });
 
-    const RootQuery = new GraphQLObjectType({
-        name: "RootQueryType",
+    const MemberType = new GraphQLObjectType({
+        name: "MemberType",
         fields: {
-            users: {
-                type: new GraphQLList(UserType),
-                resolve: async () => {
-                    return await prisma.user.findMany();
-                },
-            },
-        },
+            id: {type: GraphQLString},
+            discount: {type: GraphQLFloat},
+            postsLimitPerMonth: {type: GraphQLInt}
+        }
+    });
+
+    const Post = new GraphQLObjectType({
+        name: "Post",
+        fields: {
+            id: {type: UUID},
+            title: {type: GraphQLString},
+            content: {type: GraphQLString},
+            authorId: {type: UUID} // Связь с User
+        }
+    });
+
+    const Profile = new GraphQLObjectType({
+        name: "Profile",
+        fields: {
+            id: {type: UUID},
+            isMale: {type: GraphQLString},
+            yearOfBirth: {type: GraphQLInt},
+            memberType: {type: MemberType}
+        }
+    });
+
+    const User = new GraphQLObjectType({
+        name: "User",
+        fields: () => ({
+            id: {type: UUID},
+            name: {type: GraphQLString},
+            balance: {type: GraphQLFloat},
+            profile: {type: Profile},
+            posts: {type: new GraphQLList(Post)},
+            userSubscribedTo: {type: new GraphQLList(User)},
+            subscribedToUser: {type: new GraphQLList(User)}
+        })
     });
 
     const schema = new GraphQLSchema({
-        query: RootQuery,
+        // query: RootQuery,
     });
 
     fastify.route({
